4780b954141d776f4e929e662615215652591ade
atata174 <razhapov_albert@mail.ru>
2021-03-30 17:24:12 +0300

    Initial Commit

---
+++ RGBPickerSwiftUI/RGBPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBPickerSwiftUIApp.swift
+//  RGBPickerSwiftUI
+//
+//  Created by PenguinRaja on 30.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct RGBPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
